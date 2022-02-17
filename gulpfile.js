d79419838c53b1765c728daf8d8bea6e65ba949a
VasilyBrin <mail@profitdata.ru>
2022-02-17 22:45:06 +0300

    Enabling plugins for the javascript handler

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
 const del = require('del');
 
 const paths = {
@@ -33,14 +36,26 @@ function styles() {
     .pipe(gulp.dest(paths.styles.dest))
 }
 
-function watch() {
-  gulp.watch(paths.styles.src, styles)
+function scripts() {
+	return gulp.src(paths.scripts.src, {
+		sourcemaps: true
+	})
+	.pipe(babel())
+	.pipe(uglify())
+	.pipe(concat('main.min.js'))
+	.pipe(gulp.dest(paths.scripts.dest))
 }
 
-const build = gulp.series(clean, styles, watch)
+function watch() {
+  gulp.watch(paths.styles.src, styles)
+  gulp.watch(paths.scripts.src, scripts);
+}
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
 
 exports.clean = clean;
 exports.styles = styles;
+exports.scripts = scripts;
 exports.watch = watch;
 exports.build = build;
 exports.default = build;
